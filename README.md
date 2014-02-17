be56099a03bad7b8d9b74771c0d4fe15fc710abd
Felipe <falmeida1988@gmail.com>
2014-02-16 22:28:44 -0300

    Create skeleton code for some normalizers for some expected datatypes to be found in data sets.

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ ml201401
  This includes some helpers to manipulate files, clean data and so on.
  
 ###TODO
-  - Unit testing for functions using sample files.
-  - updating the line count function to try and guess whether some lines are headers (shouldn't count)
+  - Unit test functions using sample files.
+  - Update the line count function to try and guess whether some lines are headers (shouldn't count) (or else modify the partitioner to disregard lines that look like Headers). I think that one way to spot headers would be to look for lines where all attributes are strings
 
  
 **P.S.:** This is a WIP (work in progress). Keep in mind that I had no prior Python experience before doing this. One of the aims of this project is precisely to get me some Python experience.
---
+++ helpers/normalizers.py
@@ -0,0 +1,41 @@
+
+#return a function that should be applied to each attribute in order to "normalize" it
+def get_normalizer_for(l):
+
+	type=get_type(l)
+
+	if type == 'string':
+		return _string_normalizer(l)
+	elif type == 'number':
+		return _number_normalizer(l)
+	elif type == 'date':
+		return _date_normalizer(l)
+	else:
+		raise Exception('Unknown type for attribute list given')
+
+#TODO unit test this
+def _is_number(s):
+	try:
+    	float(s)
+        return True
+    except ValueError:
+        return False
+
+#unless all attributes are of a single type (e.g. number), type is string
+#should we define a threshold to account for bad data (e.g. a list where every element is a number except for something like "324W" - this is probably noise, I shouldn't consider everything to be strings just because of this.)
+def _get_type(l):
+
+	return 'string'
+
+#convert everything to lower case or upper case
+#remove special characters?
+#disregard whitespace/newline?
+#remove quotes, inverted commas around text
+def _string_normalizer(l):
+
+
+#normalize to (MAX_VALUE - MIN_VALUE)
+def _number_normalizer(l):
+
+#convert everything to a single Format
+def _date_normalizer(l)
