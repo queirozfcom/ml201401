050835fa740ca47691be2d896690f643278d9cee
Felipe <falmeida1988@gmail.com>
2014-04-21 02:25:00 -0300

    Store train_set into a matrix.

--- helpers/files.py
+++ helpers/files.py
@@ -1,4 +1,6 @@
 from helpers.types import is_number
+from config.constants import *
+import sys
 
 def file_len(fname):
     num_headers = 0
@@ -8,7 +10,7 @@ def file_len(fname):
             if is_header(line):
                 num_headers += 1
 
-
+    # starts at 0            
     return idx + 1 - num_headers
 
 def is_header(line):
@@ -17,4 +19,22 @@ def is_header(line):
             return False
 
     return True
+
+def num_attributes(fname):
+    enough_lines = 10
+    separator = ','
+    #TODO check whether this works when file is < 10 lines long
+
+    with open(fname) as f:
+        for idx, line in enumerate(f):
+            if idx == 0:
+                num_attributes = len(line.split(separator))
+            elif idx >= enough_lines:
+                return num_attributes
+            else:
+                if len(line.split(separator)) != num_attributes:
+                    print ERROR+" Not all lines have the same number of attributes in file "+fname+"\n"
+                    sys.exit()
+                
+
 
--- helpers/normalizers.py
+++ helpers/normalizers.py
@@ -1,7 +1,12 @@
 import helpers.types as types
 
+# TODO unit test this
 
-#return a function that should be applied to each attribute in order to "normalize" it
+# just apply the returned normalizer for each element. Simple, uh?
+def normalize(l):
+    return map(get_normalizer_for(l),l)
+
+
 def get_normalizer_for(l):
 
     type=get_type(l)
@@ -15,15 +20,15 @@ def get_normalizer_for(l):
     else:
         raise Exception('Unknown type for attribute list given')
 
-#TODO unit test this
-
-#unless all attributes are of a single type (e.g. number), type is string
-#should we define a threshold to account for bad data (e.g. a list where every element is a number except for something like "324W" - this is probably noise, I shouldn't consider everything to be strings just because of this.)
-def _get_type(line):
-    
-    if all(is_number(el) for el in line.split(',')):
+# unless all attributes are of a single type (e.g. number), type is string.
+# should we define a threshold to account for bad data? (e.g. a list where 
+#    every element is a number except for something like "324W" - this is 
+#    probably noise, I shouldn't consider everything to be strings just bec
+#    ause of this.)
+def _get_type(l):
+    if all(is_number(el) for el in l):
         return 'number'
-    elif all(is_date(el) for el in line.split(',')):
+    elif all(is_date(el) for el in l):
         return 'date'
     else:
         return 'string'
@@ -32,13 +37,13 @@ def _get_type(line):
 #remove special characters?
 #disregard whitespace/newline?
 #remove quotes, inverted commas around text
-def _string_normalizer(line):
-    return lambda line: return line
+def _string_normalizer(l):
+    return lambda element: return element
 
 #normalize to (MAX_VALUE - MIN_VALUE)
 def _number_normalizer(line):
-    return lambda line: return line
+    return lambda element: return element
 
 #convert everything to a single date Format
 def _date_normalizer(line):
-    return lambda line: return line
+    return lambda element: return element
