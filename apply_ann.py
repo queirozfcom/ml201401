2e8d08e4e1e5519caf517426f69af1dedda94f4a
Felipe <falmeida1988@gmail.com>
2014-04-26 18:18:27 -0300

    More edits..

--- apply_ann.py
+++ apply_ann.py
@@ -15,6 +15,28 @@ import sys
 import os
 import time
 
-print ERROR+""" NOT implemented yet.\n"""
+if len(sys.argv) < 2:
+    print ERROR+""" Please provide the directory where I can 
+      find test and train partitions (they should be called "test_set.csv" and 
+      "train_set".csv). Optionally, you can also set config options in config/
+      constants.py\n"""
+    sys.exit()
 
-sys.exit()
+dir_name = sys.argv[1].lstrip('.').rstrip('/').strip(" ")
+
+if not os.path.isfile(dir_name+'/train_set.csv'):
+    print ERROR+" File "+dir_name+"/train_set.csv not found.\n"
+    sys.exit()
+
+if not os.path.isfile(dir_name+'/test_set.csv'):
+    print ERROR+" File "+dir_name+"/test_set.csv not found.\n"
+    sys.exit()
+
+train_set_file = dir_name+'/train_set.csv'
+
+train_set = files.load_into_matrix(train_set_file,skip_first=False)
+
+# we won't load the targets because they're what we're trying to predict
+prediction_set = files.load_into_matrix(dir_name+'/test_set.csv',skip_first=False,num_attributes=NUM_ATTRS,load_targets=False,num_targets=NUM_TARGETS)
+
+indexes_to_use = filter(lambda x: False if x in EXCLUDE_ATTRS else True,np.arange(NUM_ATTRS))
